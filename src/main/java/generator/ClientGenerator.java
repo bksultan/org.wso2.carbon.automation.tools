6981b540616d62009d6da5ac5dca0d2c99fb79d5
ruks <ruky68@live.com>
2014-08-10 21:12:15 +0530

    added ClientGenerator javadoc and comment

--- src/main/java/generator/ClientGenerator.java
+++ src/main/java/generator/ClientGenerator.java
@@ -28,18 +28,26 @@ import com.predic8.wsdl.WSDLParser;
 
 public class ClientGenerator {
 	static Logger log = Logger.getLogger(ClientGenerator.class.getName());
-	static ArrayList<String> operations;
-	private static String path = "/src/main/java";
-	private static String packageName;
-	private static Document doc;
-	private static STGroup group;
+	static ArrayList<String> operations;				// keep operation of service listed as wsdl
+	private static String path = "/src/main/java";		// path of client should be generated
+	private static String packageName;					// keep the package name
+	private static Document doc;						// xml reading document
+	private static STGroup group;						// String template group file instance
 
+	/**
+	 * Constructor- define and import resources
+	 */
 	public ClientGenerator() {
+		//load the service.xml 
 		File pomfile = new File("src/main/resources/service.xml");
 		try {
 			DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
 					.newDocumentBuilder();
+			//init service.xml for start reading content
 			doc = dBuilder.parse(pomfile);
+			
+			//Load the string template group file. it contain template 
+			//for generate WSO2 Client library
 			group = new STGroupFile("src/main/resources/templateR.stg");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -49,47 +57,70 @@ public class ClientGenerator {
 		
 	}
 
+	/**
+	 * main method. start generate java class library
+	 * 
+	 * @param args args[0] as package name 
+	 */
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
+		
+		//new instance
 		ClientGenerator g = new ClientGenerator();
+		
 //		 g.GenerateLibraries("robotlib");
+		
+		//if no argument assume generate to default package
 		if (args.length == 0) {
 			System.out.println("INFO: using default package");
 			g.GenerateLibraries();
-		} else if (args.length > 0) {
+		} else if (args.length > 0) { // args[0] is take as package name 
 			g.GenerateLibraries(args[0]);
 		}
 
 	}
 
+	/**
+	 * set the package name
+	 * 
+	 * @param name package name
+	 */
 	public void setPackageName(String name) {
 		packageName = name;
 	}
 
+	/**
+	 * save file on disk with provided content and name. 
+	 * Library.java is append to the class name
+	 * 
+	 * @param className filename to be saved
+	 * @param result content to be saved
+	 */
 	private static void save(String className, String result) {
 		try {
-			String loc = System.getProperty("user.dir");
+			String loc = System.getProperty("user.dir");// get the current Directory
 			System.out.println("user dir: " + loc);
-			// File ff1 = new File(loc + path);
-			// if (!ff1.exists()) {
-			// ff1.mkdir();
-			// log.debug("Client Gen: Create " + ff1.getAbsolutePath());
-			// }
 			String pName;
 			if (packageName != null) {
-				pName = packageName.replaceAll("\\.","/") + "/";
+				//package name format is robot.lib, to save create directory path 
+				//replacing "." with "/"
+				pName = packageName.replaceAll("\\.","/") + "/"; 
 			} else {
 				pName = "";
 			}
 
+			//create file with package path
 			File ff2 = new File(loc + path + "/" + pName);
 			if (!ff2.exists()) {
-				ff2.mkdirs();
+				ff2.mkdirs();//if directory not exist create
 				log.debug("Client Gen: Create " + ff2.getAbsolutePath());
 			}
 
+			//create java file to be saved
 			File f = new File(loc + path + "/" + pName + className
 					+ "Library.java");
+			
+			//save to the disk
 			BufferedWriter wri = new BufferedWriter(new FileWriter(f));
 			wri.write(result);
 			wri.close();
@@ -102,6 +133,13 @@ public class ClientGenerator {
 		}
 	}
 
+	/**
+	 * check the operation is available in the WSDL as web service operation.
+	 * name is the method name in the stub
+	 * 
+	 * @param name method name
+	 * @return
+	 */
 	private static boolean isMethodNameValid(String name) {
 
 		if (operations.contains(name)) {
@@ -112,8 +150,14 @@ public class ClientGenerator {
 
 	}
 
+	/**
+	 * check the parameters, return type of any class is primitive type.
+	 * if class name is primitive, return as false
+	 * 
+	 * @param name class name
+	 * @return true if class is not primitive
+	 */
 	private static boolean isNameValid(String name) {
-		// System.out.println(name);
 		String[] type = { "int", "boolean", "String", "Class", "Object",
 				"void", "java.lang.String", "double", "long" };
 		for (String s : type) {
@@ -125,11 +169,19 @@ public class ClientGenerator {
 		return true;
 	}
 
-	private static ArrayList<String> getOperations(Class<?> c, String wsdl) {
+	/**
+	 * 
+	 * get the operation of the web service based on the WSDL file.
+	 * 
+	 * @param cls full class name(stub) that jar contain WSDL file
+	 * @param wsdl name of the WSDL
+	 * @return return list of opeation's name
+	 */
+	private static ArrayList<String> getOperations(Class<?> cls, String wsdl) {
 
 		ArrayList<String> li = new ArrayList<String>();
 		try {
-			InputStream input = c.getResourceAsStream(wsdl);
+			InputStream input = cls.getResourceAsStream(wsdl);
 			WSDLParser parser = new WSDLParser();
 			Definitions defs = parser.parse(input);
 			for (PortType pt : defs.getPortTypes()) {
@@ -145,9 +197,16 @@ public class ClientGenerator {
 
 	}
 
-	private static String getServiceName(Class<?> c, String wsdl) {
+	/**
+	 * get the service name of the web service based on the WSDL file.
+	 * 
+	 * @param cls full class name(stub) that jar contain WSDL file
+	 * @param wsdl name of the WSDL
+	 * @return return service name
+	 */
+	private static String getServiceName(Class<?> cls, String wsdl) {
 
-		InputStream input = c.getResourceAsStream(wsdl);
+		InputStream input = cls.getResourceAsStream(wsdl);
 		WSDLParser parser = new WSDLParser();
 		Definitions defs = parser.parse(input);
 
@@ -159,22 +218,17 @@ public class ClientGenerator {
 
 	}
 
+	/**
+	 * generate java class content using string template and save to disk
+	 * 
+	 * @param res array with res[0] is stub class name and res[1] is wsdl file name
+	 */
 	public void generateClient(String[] res) {
-		Set<String> importLib = new HashSet<String>();
-		// URL
-		// url=ClientGenerator.class.getResource("src/main/resources/templateR.stg");
-		// URL url = null;
-		// try {
-		// url = ClassLoader.getSystemResource("templateR.stg");
-		// } catch (Exception e) {
-		// System.out.println("here" + e.getMessage());
-		// }
-
-		// ClientGenerator.class.getResource("src/main/resources/templateR.stg");
-
-		// STGroup group = new STGroupFile(url,"UTF-8",'<','>');
-//		STGroup group = new STGroupFile("src/main/resources/templateR.stg");
+		// list is for keep the list of classes should be imported to the generating class
+		Set<String> importLib = new HashSet<String>(); 
+		
 		try {
+			//res should contain the stub and wsdl information
 			if (res == null) {
 				System.out.println("error: No Service named ");
 				return;
@@ -183,133 +237,134 @@ public class ClientGenerator {
 			String servicestub = res[0];
 			String wsdl = "/" + res[1] + ".wsdl";
 
-			Class<?> c = Class.forName(servicestub);
+			Class<?> c = Class.forName(servicestub); //get the class of the stub
 
 			System.out.println(c.getPackage().getName());
-			importLib.add(servicestub);
+			importLib.add(servicestub); // add the stub class to import to library later
 
 			String methods = "";
-			String poolmethods = "";
+//			String poolmethods = "";
 
-			operations = getOperations(c, wsdl);
+			operations = getOperations(c, wsdl); // get the wsdl operations
 
+			// iterate over stub methods
 			for (Method m : c.getMethods()) {
 				if (!isMethodNameValid(m.getName())) {
-					continue;
+					continue; // if method is not listed in wsdl skip it
 				}
 
-				ST methodTem = group.getInstanceOf("method");
-				methodTem.add("returnType", m.getReturnType().getSimpleName());
-				methodTem.add("methodName", m.getName());
+				ST methodTem = group.getInstanceOf("method");					// load the method template
+				methodTem.add("returnType", m.getReturnType().getSimpleName());	//	set the method return type
+				methodTem.add("methodName", m.getName());						//	set the method name
 				String retType = m.getReturnType().getSimpleName();
 				retType = retType.replace("[]", "");
-				if (isNameValid(retType)) {
-					importLib.add(m.getReturnType().getCanonicalName()
+				if (isNameValid(retType)) {										// if the return type is complex  
+					importLib.add(m.getReturnType().getCanonicalName()			// add to importLib list
 							.replace("[]", ""));
 				}
 				String paras = "";
 				String parasRet = "";
 				int i = 0;
+				
+				// iterate over operation parameters
 				for (Class<?> pc : m.getParameterTypes()) {
 					String retType1 = pc.getSimpleName();
 
 					retType1 = retType1.replace("[]", "");
-					if (isNameValid(retType1)) {
-						importLib.add(pc.getCanonicalName().replace("[]", ""));
+					if (isNameValid(retType1)) {									// check para is complex
+						importLib.add(pc.getCanonicalName().replace("[]", ""));		// if complex add to importlib list
 						System.out.println("------------------------");
 						System.out.println("complex: " + retType1);
-						// String
-						// mm=creatMethods(pc.getCanonicalName().replace("[]",
-						// ""));
-						// poolmethods+=mm;
 						System.out.println("------------------------");
 					}
-					paras += pc.getSimpleName() + " " + "arg" + (i) + ",";
-					parasRet += "arg" + (i++) + ",";
+					paras += pc.getSimpleName() + " " + "arg" + (i) + ",";			//set the method parameters with para type
+					parasRet += "arg" + (i++) + ",";								// set the para for stub method
 				}
-				if (!paras.equals("")) {
-					methodTem.add("paras",
-							paras.substring(0, paras.length() - 2));
+				if (!paras.equals("")) {									// check operation have parameters
+					methodTem.add("paras",										// if accept format string to remove last "," symbol and 
+							paras.substring(0, paras.length() - 2));				// set the template paras
 					methodTem.add("parasRet",
 							parasRet.substring(0, parasRet.length() - 2));
 
 				}
 
+				//set the is operation is return value 
 				methodTem.add("cond", !m.getReturnType().getSimpleName()
 						.equals("void"));
+				//rendor the template as string and concat all the methods of string into one
 				methods += methodTem.render();
 			}
 
-			methods += poolmethods;
+//			methods += poolmethods;
 
-			ST classTem = group.getInstanceOf("class");
+			ST classTem = group.getInstanceOf("class");			//load the class template
 
-			String serviceName = getServiceName(c, wsdl);
+			String serviceName = getServiceName(c, wsdl);		// get the service name of wsdl
 			System.out.println("className " + serviceName);
 
-			classTem.add("name", serviceName);
-			classTem.add("namestub", serviceName + "Stub");
+			classTem.add("name", serviceName);					//set the class name as service name
+			classTem.add("namestub", serviceName + "Stub");		//set the service stub class name
 			classTem.add("methods", methods);
 
-			if (packageName != null) {
+			// if package name has set, set the class package name 
+			if (packageName != null) {				
 				classTem.add("package", "package " + packageName + ";");
 			}
 
+			//iterate over list of class that should import, format
 			String imports = "";
 			for (String s : importLib) {
 				imports += "import " + s + ";\n";
 			}
 
-			classTem.add("clsimport", imports);
+			classTem.add("clsimport", imports);					//set the imported class
 
-			save(serviceName, classTem.render());
+			save(serviceName, classTem.render());				// save the template into file
 			log.debug("info: Client generated " + serviceName);
 		} catch (ClassNotFoundException e) {
 			System.out.println("error " + e.getMessage());
 		}
 	}
 
+	/**
+	 * default method to generate libraries. Assume package is default package.
+	 *  generate libraries based on the services.xml
+	 */
 	public void GenerateLibraries() {
 		GenerateLibraries(null);
 	}
-
+	
+	/**
+	 * generate libraries based on the services.xml
+	 * @param pName package name of the java library classes
+	 */
 	public void GenerateLibraries(String pName) {
 
 		if (pName == "" || pName == null) {
-			setPackageName(null);
+			setPackageName(null);				// if name is empty assume package is default 
 		} else {
 			setPackageName(pName);
 		}
 
 		// PropertyConfigurator.configure("src/main/resources/log4j.properties");
 
-		File f = new File("/home/rukshan/log4j/log.out");
-		f.delete();
+		//File f = new File("/home/rukshan/log4j/log.out");
+		//f.delete();
 
-		// InputStream
-		// s=ClientGenerator.class.getResourceAsStream("src/main/resources/service.xml");
-		// InputStream
-		// s=getClass().getResourceAsStream("src/main/resources/service.xml");
-		// File pomfile = new File("src/main/resources/service.xml");
-		// InputStream s=ClassLoader.getSystemResourceAsStream("service.xml");
 		String[] res;
 		try {
 
-			// DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
-			// .newDocumentBuilder();
-			// Document doc = dBuilder.parse(pomfile);
-			// Document doc = dBuilder.parse(s);
 			System.out.println("Root element :"
 					+ doc.getDocumentElement().getNodeName());
 
-			NodeList service = doc.getElementsByTagName("service");
-			for (int i = 0; i < service.getLength(); i++) {
+			NodeList service = doc.getElementsByTagName("service");		// read the service.xml and load service tag
+			for (int i = 0; i < service.getLength(); i++) {				// iterate over list of service tag
 				Element ele = (Element) service.item(i);
 				res = new String[2];
-				res[0] = ele.getAttribute("stub");
-				res[1] = ele.getAttribute("wsdl");
+				res[0] = ele.getAttribute("stub");						// get the stub attribute
+				res[1] = ele.getAttribute("wsdl");						// get the wsdl attribute
 
-				generateClient(res);
+				generateClient(res);									// generate library and save 
 
 			}
 			log.debug("Standard class: Client Generated");
